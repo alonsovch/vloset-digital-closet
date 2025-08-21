053f31ed2334c50931a289fb8c78a157a7fbe35f
Alonso Herrera <alonso.herrera@ug.uchile.cl>
2025-08-21 19:45:21 -0400

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755819921105

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7137d75d._.js",
+  "static/chunks/[root-of-the-server]__1a9f3e9a._.js",
+  "static/chunks/[root-of-the-server]__7db745e8._.css"
+],
+    source: "dynamic"
+});
